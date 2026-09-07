5bb3d655fe6f35e488f3e32bff742ad9da7fb394
Jonathan JEANNIARD <jonathan.jeanniard@2isa.org>
2026-09-07 11:53:32 +0200

    Ajout de la méthode electionsLegislatives
    Ajout d'une méthode pour simuler une élection législative :
    - Collecte des scores de quatre candidats via la console.
    - Vérification si un candidat obtient plus de 50 % des voix.
    - Ajout d'un commentaire TODO pour la logique du second tour.
    - Réorganisation des commentaires pour plus de clarté.

--- S1_Algo/GeneralElections.cs
+++ S1_Algo/GeneralElections.cs
@@ -8,7 +8,32 @@ namespace S1_Algo
 {
     internal class GeneralElections
     {
-        //Si un candidat obtient plus de 50% des voix, il est élu.
-        //Si aucun candidat n'obtient plus de 50% des voix, un second tour est organisé entre les candidats ayant obtenu au moins 12,5% des voix au premier tour.
+        public static void electionsLegislatives()
+        {
+            string result = "Favorable";
+            double[] scoreCandidate = new double[4];
+
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine($"Entrez le score du candidat {i + 1} :");
+                scoreCandidate[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            
+            //Si un candidat obtient plus de 50% des voix, il est élu.
+            //Si aucun candidat n'obtient plus de 50% des voix, un second tour est organisé entre les candidats ayant obtenu au moins 12,5% des voix au premier tour.
+
+            for (int i = 0; i <= 3; i++)
+            {
+                if (scoreCandidate[i] > 50)
+                {
+                    result = $"Le candidat {i + 1} est élu.";
+                    break;
+                }
+                else
+                {
+                    //TODO: Implement second round logic
+                }
+            }
+        }
     }
 }
