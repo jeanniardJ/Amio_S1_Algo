5a7a2c3bd4e79fd0a29051720bb7beea5e0f7776
Jonathan JEANNIARD <jonathan.jeanniard@2isa.org>
2026-09-07 09:22:40 +0200

    Refactorisation logique et ajout d'affichage des exercices
    Ajout d'une logique conditionnelle dans `ProduitOfNbr.cs` pour déterminer le signe du produit de deux nombres (positif, négatif ou nul) avec des instructions `if-else`. Suppression de l'ancien affichage statique.
    
    Ajout d'une liste d'exercices dans `Program.cs` pour afficher dynamiquement les options disponibles à l'utilisateur avec une boucle `for`. Ajout d'une option pour quitter le programme.
    
    Simplification des `case` dans le `switch` de `Program.cs` en supprimant les messages explicites tout en conservant les appels aux méthodes associées.

--- S1_Algo/ProduitOfNbr.cs
+++ S1_Algo/ProduitOfNbr.cs
@@ -8,10 +8,24 @@
             int nbr1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Rentrez un deuxième nombre : ");
             int nbr2 = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+            //string result = (nbr1 > 0 && nbr2 > 0) ? "Le produit est positif" : (nbr1 < 0 && nbr2 < 0) ? "Le produit est positif" : "Le produit est négatif";
 
-            Console.WriteLine("Le signe du produit est positif");
+            if ((nbr1 > 0 && nbr2 > 0) || (nbr1 < 0 && nbr2 < 0))
+            {
+                result = "Le produit est positif";
+            }
+            else if(nbr1 == 0 || nbr2 == 0)
+            {
+                result = "Le produit est zéro";
+            }
+            else
+            {
+                result = "Le produit est négatif";
+            }
 
-            Console.WriteLine("Le signe du produit est négatif");
+            Console.WriteLine($"Le produit de {nbr1} et {nbr2} est : {result}");
+
         } 
     }
 }
--- S1_Algo/Program.cs
+++ S1_Algo/Program.cs
@@ -28,8 +28,30 @@ namespace S1_Algo
             int input;
             Console.WriteLine("Choisissez un exercice de 1 à .... : ");
 
-            //TODO : Faire un affichage des exercices disponibles avec leur numéro correspondant
+            String[] exos = new String[]
+            {
+                "1). Pair ou impair",
+                "2). Le plus grand des deux",
+                "3). Signe d'un nombre",
+                "4). Note",
+                "5). Signe d'un produit",
+                "6). Réponse",
+                "7). Catégorie",
+                "8). L'impôt sur Zoeglub",
+                "9). Facture photocopies",
+                "10). Assurance automobile",
+                "11). Prédire l'avenir",
+                "12). Les élections législatives",
+                "13). Vérification d'une date"
+            };
 
+            for (int i = 0; i < exos.Length; i++)
+            {
+                Console.WriteLine($"Exercice " + exos[i]);
+            }
+
+            Console.WriteLine("Ou taper le nombre 0 pour quitter le programme.");
+            
             try
             {
                 input = Convert.ToInt32(Console.ReadLine());
@@ -45,51 +67,38 @@ namespace S1_Algo
             switch (inputChoix)
             {
                 case 1:
-                    Console.WriteLine("Exercice pair ou impair");
                     Modulo.modulo();
                     break;
                 case 2:
-                    Console.WriteLine("Exercice le plus grand des deux");
                     FindUpNumber.findUpNumber();
                     break;
                 case 3:
-                    Console.WriteLine("Exercice signe d'un nombre");
                     SignOfNb.signOfnumber();
                     break;
                 case 4:
-                    Console.WriteLine("Exercice note");
                     Note.note();
                     break;
                 case 5:
-                    Console.WriteLine("Exercice signe d'un produit");
-
+                    ProduitOfNbr.produitIfPositiveNegative();
                     break;
                 case 6:
-                    Console.WriteLine("Exercice Réponse");
 
                     break;
                 case 7:
-                    Console.WriteLine("Exercice Catégorie");
 
                     break;
                 case 8:
-                    Console.WriteLine("Exercice l'impôt sur Zoeglub");
 
                     break;
                 case 9:
-                    Console.WriteLine("Exercice facture photocopies");
                     break;
                 case 10:
-                    Console.WriteLine("Exercice assurance automobile");
                     break;
                 case 11:
-                    Console.WriteLine("Exercice Prédire l'avenir");
                     break;
                 case 12:
-                    Console.WriteLine("Exercice les élections législatives");
                     break;
                 case 13:
-                    Console.WriteLine("Exercice vérification d'une date");
                     break;
                 default:
                     Console.WriteLine("Aucun exercice choisi");
