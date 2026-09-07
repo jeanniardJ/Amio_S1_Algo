6981baf0ea464373a4cd890867a474f06802f363
Jonathan JEANNIARD <jonathan.jeanniard@2isa.org>
2026-09-07 09:08:27 +0200

    Ajout boucle et gestion erreurs dans ChoixExo
    Ajout d'une boucle pour permettre à l'utilisateur de continuer ou non après l'exécution d'un exercice.
    Ajout d'une gestion des erreurs dans la méthode `ChoixExo` pour valider les entrées utilisateur avec un bloc `try-catch`.
    Ajout de nouveaux cas (2 à 13) dans le `switch` de `ChoixExo`, avec des messages spécifiques et des appels à des méthodes associées pour certains exercices.
    Ajout d'un commentaire `//TODO` pour signaler l'affichage des exercices disponibles à implémenter.
    Certains cas (5 à 13) ne sont pas encore complètement implémentés.

--- S1_Algo/Program.cs
+++ S1_Algo/Program.cs
@@ -12,8 +12,11 @@ namespace S1_Algo
             while (true)
             {
                 ChoixExo();
+
                 Console.WriteLine("Voulez-vous continuer ? (O/N)");
+
                 string inputContinue = Console.ReadLine();
+
                 if (inputContinue.ToUpper() != "O")
                 {
                     break;
@@ -22,8 +25,22 @@ namespace S1_Algo
         }
         static void ChoixExo()
         {
+            int input;
             Console.WriteLine("Choisissez un exercice de 1 à .... : ");
-            int inputChoix = Convert.ToInt32(Console.ReadLine());
+
+            //TODO : Faire un affichage des exercices disponibles avec leur numéro correspondant
+
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Veuillez entrer un nombre valide.");
+                return;
+            }
+
+            int inputChoix = Convert.ToInt32(input);
 
             switch (inputChoix)
             {
@@ -32,7 +49,47 @@ namespace S1_Algo
                     Modulo.modulo();
                     break;
                 case 2:
+                    Console.WriteLine("Exercice le plus grand des deux");
+                    FindUpNumber.findUpNumber();
+                    break;
+                case 3:
+                    Console.WriteLine("Exercice signe d'un nombre");
+                    SignOfNb.signOfnumber();
+                    break;
+                case 4:
+                    Console.WriteLine("Exercice note");
+                    Note.note();
+                    break;
+                case 5:
+                    Console.WriteLine("Exercice signe d'un produit");
 
+                    break;
+                case 6:
+                    Console.WriteLine("Exercice Réponse");
+
+                    break;
+                case 7:
+                    Console.WriteLine("Exercice Catégorie");
+
+                    break;
+                case 8:
+                    Console.WriteLine("Exercice l'impôt sur Zoeglub");
+
+                    break;
+                case 9:
+                    Console.WriteLine("Exercice facture photocopies");
+                    break;
+                case 10:
+                    Console.WriteLine("Exercice assurance automobile");
+                    break;
+                case 11:
+                    Console.WriteLine("Exercice Prédire l'avenir");
+                    break;
+                case 12:
+                    Console.WriteLine("Exercice les élections législatives");
+                    break;
+                case 13:
+                    Console.WriteLine("Exercice vérification d'une date");
                     break;
                 default:
                     Console.WriteLine("Aucun exercice choisi");
