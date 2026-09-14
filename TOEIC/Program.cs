4dcc97be7dc66a91b4819d930c9a3120241b23b7
Jonathan JEANNIARD <jonathan.jeanniard@2isa.org>
2026-09-14 11:55:43 +0200

    Ajout du projet S1_Algo_Exo_TOEIC à la solution
    Un nouveau projet **S1_Algo_Exo_TOEIC** a été ajouté à la solution `S1_Algo.sln`.
    - Mise à jour des configurations de build (Debug/Release) dans le fichier de solution.
    - Ajout d'une application console dans `Program.cs` pour collecter et afficher le nom et le score TOEIC de l'utilisateur avec des messages adaptés.
    - Création du fichier `S1_Algo_Exo_TOEIC.csproj` utilisant le SDK .NET 8.0 avec les options `ImplicitUsings` et `Nullable` activées.

---
+++ TOEIC/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TOEIC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            string userName = retrieveUserName();
+            int userTOIECScore = retrieveTOEICScore();
+
+            printWelcomMessage(userName, userTOIECScore);
+
+            string retrieveUserName()
+            {
+                Console.Write("Veuillez saisir votre nom : ");
+                return Console.ReadLine();
+            }
+
+            int retrieveTOEICScore()
+            {
+                bool result;
+                int nombre;
+
+                do
+                {
+                    Console.WriteLine("Veuillez saisir votre score au TOEIC : ");
+                    string inputUser = Console.ReadLine();
+                    
+                    result = int.TryParse(inputUser, out nombre) ? true : false;
+
+                    Console.WriteLine($"Votre score n'est pas valide");
+                } while (!result);
+
+                return nombre;
+            }
+
+            void printWelcomMessage(string userName, int userTOIECScore)
+            {
+                Console.WriteLine("*****************\r\n** Bienvenue ! **\r\n*****************");
+                Console.WriteLine($"Bonjour {userName}.");
+                Console.WriteLine("Nous sommes ravies de t'accuillir parmis nous.");
+                GetToeicFeedbackMessage(userTOIECScore);
+            }
+
+            void GetToeicFeedbackMessage(int userTOIECScore)
+            {
+                switch (userTOIECScore)
+                {
+                    case < 650:
+                        Console.WriteLine("Ne vous découragez pas le TOEIC est une épreuve qui nécessite de la ténacité !");
+                        break;
+                    case < 850:
+                        Console.WriteLine($"Félicitations pour votre score de {userTOIECScore} au TOIEC \u263B");
+                        break;
+                    case >= 850:
+                        Console.WriteLine($"Incroyable ! Félicitations pour votre score de {userTOIECScore} au TOIEC");
+                        break;
+                }
+            }
+        }
+
+    }
+}
