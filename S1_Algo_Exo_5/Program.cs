283cea8cf196b03b9f1f426dea1d8d29802f4880
Jonathan JEANNIARD <jonathan.jeanniard@2isa.org>
2026-09-23 14:25:12 +0200

    Amélioration du tirage aléatoire de noms
    Ajout d'une boucle dans Program.cs pour exécuter 1000 tirages
    de noms aléatoires. Introduction d'un tableau statique
    `nomsDisponibles` pour simplifier le tirage sans entrée
    utilisateur. Remplacement de la méthode `randomName` par une
    version améliorée permettant de tirer plusieurs noms tout en
    évitant les doublons. Mise à jour de `DrawRandomName()` pour
    afficher les résultats formatés. Ajout de documentation XML
    dans RandomNamePicker.cs pour améliorer la lisibilité et la
    maintenabilité du code.

--- S1_Algo_Exo_5/Program.cs
+++ S1_Algo_Exo_5/Program.cs
@@ -27,8 +27,13 @@
             //Console.WriteLine("\n");
             //CaesarCipher.EncryptMessage("bonjour et bienvenu en algo, les animaux sont dans un zoo", 3);
             Console.WriteLine("\n");
+
             Console.WriteLine("\n");
-            RandomNamePicker.DrawRandomName();
+            for(int n = 0; n < 1000; n++)
+            {
+                RandomNamePicker.DrawRandomName();
+            }
+            
         }
     }
 }
--- S1_Algo_Exo_5/RandomNamePicker.cs
+++ S1_Algo_Exo_5/RandomNamePicker.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace S1_Algo_Exo_5
 {
+    /// <summary>
+    /// Provides functionality to select random names from a predefined or user-supplied list.
+    /// </summary>
     internal class RandomNamePicker
     {
+        static string[] nomsDisponibles = { "Alice", "Bob", "Charlie", "David", "Eve",
+                                     "Frank", "Grace", "Hank", "Ivy", "Jack",
+                                     "Kate", "Leo", "Mia", "Nick", "Olivia",
+                                     "Paul", "Quinn", "Rose", "Sam", "Tina" };
+
+
         public static void DrawRandomName()
         {
-            string[] noms = inputName();
+            //string[] noms = inputName();
 
             //Tirer au sort un nom, puit donner la possibiliter à l'user de définir le nombre de nom tirer au sort
-            randomName(noms);
+            string[] nomTirer = randomName(nomsDisponibles, 3);
+
+            Console.WriteLine($"Les noms tirer au sort sont :");
+
+            for(int n = 0; n < nomTirer.Length; n++)
+            {
+                Console.WriteLine($"Le {n + 1} est {nomTirer[n]}");
+            }
         }
 
+        /// <summary>
+        /// Demande à l'utilisateur d'entree 12 nom, et verifier si il n'est pas déja enregistrer.
+        /// </summary>
+        /// <returns>Tableau de nom</returns>
         static string[] inputName()
         {
             string[] noms = new string[12];
@@ -50,9 +71,36 @@ namespace S1_Algo_Exo_5
             return noms;
         }
 
-        static void randomName(string[] names)
+        /// <summary>
+        /// Tirer au sort le nom d'un ou plusieurs utilisateurs
+        /// </summary>
+        /// <param name="names">Tableau de noms</param>
+        /// <param name="nbr">Nombre d'utilisateur à tirer au sort</param>
+        static string[] randomName(string[] names, int nbr)
         {
+            Random rnd = new();
+            string[] nomTirer = new string[nbr];
+            string nom;
+            bool isFind = false;
 
+            //Verifier que l'utilisateur n'a pas été tirer au sort 2, sinon relancer le tirage au sort
+            for (int n = 0; n < nbr; n++)
+            {
+                nom = names[rnd.Next(names.Length)];
+
+                for (int y = 0; y < nomTirer.Length; y++)
+                {
+                    if (nomTirer[y] == nom)
+                    {
+                        Console.WriteLine("Ce nom est déja tirer au sort!");
+                        n = 0;
+                    }
+                }
+
+                nomTirer[n] = nom;
+            }
+
+            return nomTirer;
         }
     }
 }
